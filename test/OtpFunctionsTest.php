de3332f6af9f631915415bb40b7993133006df2d
svn0202 <svn0202@users.noreply.github.com>
2026-08-09 13:51:12 +0500

    Type Base32 decoding contract

---
+++ test/OtpFunctionsTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Test;
+
+use PHPUnit\Framework\TestCase;
+
+require_once __DIR__ . '/../shared/code/tce_functions_otp.php';
+
+final class OtpFunctionsTest extends TestCase
+{
+    public function testBase32DecoderMatchesKnownValue(): void
+    {
+        self::assertSame('foo', \F_decodeBase32('MZXW6==='));
+    }
+
+    public function testOtpMatchesRfc6238Sha1VectorAt59Seconds(): void
+    {
+        self::assertSame(287082, \F_getOTP('GEZDGNBVGY3TQOJQGEZDGNBVGY3TQOJQ', 59));
+    }
+}
