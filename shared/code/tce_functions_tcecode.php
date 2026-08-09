32082c6d2775ee3f7e5cd770298b74ecf8e15b72
svn0202 <svn0202@users.noreply.github.com>
2026-08-09 19:30:53 +0500

    Declare LaTeX callback return type

--- shared/code/tce_functions_tcecode.php
+++ shared/code/tce_functions_tcecode.php
@@ -525,7 +525,7 @@ function F_tcecode_run_process(array $command, string $working_directory): array
  * @param $matches (string) array containing matches: $matches[0] is the complete match, $matches[1] the match for the first subpattern enclosed in '(...)' (the LaTeX code)
  * @return string replacement HTML code string to include the equivalent LaTeX image.
  */
-function F_latex_callback($matches)
+function F_latex_callback($matches): mixed
 {
     require_once '../../shared/config/tce_latex.php';
     $picture_path = (string) K_LATEX_PATH_PICTURE;
