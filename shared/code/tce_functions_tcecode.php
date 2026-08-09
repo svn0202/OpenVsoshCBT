63c5bc85893f6fd61bd7089dd4c19158d6adf22d
svn0202 <svn0202@users.noreply.github.com>
2026-08-09 20:05:20 +0500

    Declare TCECode removal parameter type

--- shared/code/tce_functions_tcecode.php
+++ shared/code/tce_functions_tcecode.php
@@ -910,7 +910,7 @@ function F_objects_replacement(mixed $name, mixed $extension, mixed $width = 0, 
  * @param $str (string) text to process
  * @return string without tcecode markup tags
  */
-function f_remove_tcecode($str): mixed
+function f_remove_tcecode(mixed $str): mixed
 {
     $str = preg_replace("'\[object\](.*?)\[/object([^\]]*?)\]'si", '[OBJ]', $str);
     $str = preg_replace("'\[img([^\]]*?)\](.*?)\[/img\]'si", '[IMG]', $str);
