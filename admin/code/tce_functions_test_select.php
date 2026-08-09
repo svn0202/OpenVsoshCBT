a13f05b4f3f25a2f62164e873d5f13db0c5ff340
svn0202 <svn0202@users.noreply.github.com>
2026-08-09 23:45:21 +0500

    Preserve legacy test selection flag

--- admin/code/tce_functions_test_select.php
+++ admin/code/tce_functions_test_select.php
@@ -239,7 +239,7 @@ function F_show_select_test($order_field, $orderdir, $firstrow, $rowsperpage, $a
                         . $l['w_select']
                         . '"'
                 ;
-                if (isset($_REQUEST['checkall']) && $_REQUEST['checkall'] == 1) {
+                if (isset($_REQUEST['checkall']) && f_legacy_int_equals($_REQUEST['checkall'], 1)) {
                     echo ' checked="checked"';
                 }
 
