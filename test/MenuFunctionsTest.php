7d3147fd5e3d34686a2689b37d872880d079824c
svn0202 <svn0202@users.noreply.github.com>
2026-08-09 20:27:28 +0500

    Declare menu link parameter types

--- test/MenuFunctionsTest.php
+++ test/MenuFunctionsTest.php
@@ -21,6 +21,29 @@ final class MenuFunctionsTest extends TestCase
         $_SERVER['SCRIPT_NAME'] = $this->originalScriptName;
     }
 
+    public function testMenuLinkRendersEnabledItem(): void
+    {
+        [$status, $output] = F_tcecode_run_process(
+            [
+                PHP_BINARY,
+                '-r',
+                'require_once "../config/tce_config.php"; require_once "tce_functions_menu.php"; '
+                    . '$GLOBALS["l"] = ["a_meta_charset" => "UTF-8"]; '
+                    . '$_SESSION["session_user_level"] = 0; $_SERVER["SCRIPT_NAME"] = "/current.php"; '
+                    . 'echo F_menu_link("other.php", ['
+                    . '"enabled" => true, "level" => 0, "link" => "/other.php", '
+                    . '"title" => "Other", "name" => "Other", "key" => "", "icon" => ""]);',
+            ],
+            dirname(__DIR__) . '/shared/code',
+        );
+
+        self::assertSame(0, $status);
+        self::assertSame(
+            '<li><a href="/other.php" title="Other"><span class="menu-label">Other</span></a></li>' . "\n",
+            $output,
+        );
+    }
+
     public function testChildActivityRecognizesDirectChild(): void
     {
         $menu = [
