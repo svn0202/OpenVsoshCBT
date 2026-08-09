29bfd251c7058555fe7d9592d423642fd9b4df16
svn0202 <svn0202@users.noreply.github.com>
2026-08-09 18:20:07 +0500

    Define file information contract

--- test/FileManagerFunctionsTest.php
+++ test/FileManagerFunctionsTest.php
@@ -8,6 +8,37 @@ require_once __DIR__ . '/../admin/code/tce_functions_filemanager.php';
 
 final class FileManagerFunctionsTest extends TestCase
 {
+    public function testReturnsFileInformationWithStableScalarFields(): void
+    {
+        [$status, $output] = F_tcecode_run_process(
+            [
+                PHP_BINARY,
+                '-r',
+                'require "tce_functions_filemanager.php"; $info = F_getFileInfo($argv[1]); echo implode("\n", ['
+                    . '$info["basename"], $info["extension"], $info["filename"], $info["dir"] ? "1" : "0", '
+                    . '(string) $info["size"], $info["link"] ? "1" : "0", $info["aperms"]]);',
+                __FILE__,
+            ],
+            __DIR__ . '/../admin/code',
+        );
+        self::assertSame(0, $status);
+
+        $fields = explode("\n", $output);
+        self::assertCount(7, $fields);
+        self::assertSame(
+            [
+                'FileManagerFunctionsTest.php',
+                'php',
+                'FileManagerFunctionsTest',
+                '0',
+                (string) filesize(__FILE__),
+                '0',
+            ],
+            array_slice($fields, 0, 6),
+        );
+        self::assertStringStartsWith('-', $fields[6] ?? '');
+    }
+
     public function testFormatsFileSizesUsingLegacyUnitsAndRounding(): void
     {
         self::assertSame('0', F_formatFileSize(0));
