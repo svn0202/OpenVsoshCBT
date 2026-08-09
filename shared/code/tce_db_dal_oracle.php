5e635b3b367d8a432a889a7b95c86b89e679f6ca
svn0202 <svn0202@users.noreply.github.com>
2026-08-10 02:51:01 +0500

    Normalize Oracle row count helper name

--- shared/code/tce_db_dal_oracle.php
+++ shared/code/tce_db_dal_oracle.php
@@ -178,7 +178,7 @@ function F_db_affected_rows($link_identifier, $result)
  * @param $result (resource) result resource to the query result.
  * @return Number of affected rows.
  */
-function F_db_num_rows($result)
+function f_db_num_rows($result)
 {
     $output = [];
     set_error_handler(static fn (): bool => true);
