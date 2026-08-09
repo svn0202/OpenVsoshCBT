3a222b2e138563c45ef63763cec74b29713cb0b5
svn0202 <svn0202@users.noreply.github.com>
2026-08-10 01:21:26 +0500

    Normalize TCECode tag converter name

--- shared/code/tce_functions_tcecode.php
+++ shared/code/tce_functions_tcecode.php
@@ -378,7 +378,7 @@ function f_tcecode_url(string $text): string
  * @param string $text
  * @return string
  */
-function F_tcecode_tag(string $text): string
+function f_tcecode_tag(string $text): string
 {
     // Patterns and replacements
     $tag = [
