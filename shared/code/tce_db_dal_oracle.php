70426e5a3050e1b1c3cffbbbfe8ff5fedbe3ae7e
svn0202 <svn0202@users.noreply.github.com>
2026-08-10 02:35:54 +0500

    Normalize Oracle datetime difference helper name

--- shared/code/tce_db_dal_oracle.php
+++ shared/code/tce_db_dal_oracle.php
@@ -217,7 +217,7 @@ function F_db_insert_id($link_identifier, $tablename = '', $fieldname = '')
  * Returns the SQL string to calculate the difference in seconds between to datetime fields.
  * @return SQL query string
  */
-function F_db_datetime_diff_seconds($start_date_field, $end_date_field)
+function f_db_datetime_diff_seconds($start_date_field, $end_date_field)
 {
     return '(' . $end_date_field . ' – ' . $start_date_field . ')*86400';
 }
