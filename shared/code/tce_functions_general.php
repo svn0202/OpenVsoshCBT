4ccf3e06c369e5ea0b18ef4d9ea470f1c91d837e
svn0202 <svn0202@users.noreply.github.com>
2026-08-09 19:22:33 +0500

    Declare HTML to TSV return type

--- shared/code/tce_functions_general.php
+++ shared/code/tce_functions_general.php
@@ -663,7 +663,7 @@ function get_data_tsv($data): string
  * @param $str (string) HTML string to convert.
  * @return string TSV
  */
-function f_html_to_tsv($str)
+function f_html_to_tsv($str): mixed
 {
     $dollar_replacement = ':.dlr.:'; //string replacement for dollar symbol
     //tags conversion table
