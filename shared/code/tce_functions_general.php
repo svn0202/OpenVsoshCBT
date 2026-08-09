00c570f1a94d0d928207e32d6e3d3a8697997dbe
svn0202 <svn0202@users.noreply.github.com>
2026-08-09 20:23:52 +0500

    Declare UTF-8 trim parameter type

--- shared/code/tce_functions_general.php
+++ shared/code/tce_functions_general.php
@@ -372,7 +372,7 @@ function show_required_field(mixed $mode = 1): string
  * @param $txt (string) The string that will be trimmed.
  * @return string The trimmed string.
  */
-function utrim($txt): mixed
+function utrim(mixed $txt): mixed
 {
     if (empty($txt)) {
         return '';
