1ded556a53c34bca4e8eeef7611289a01a97d376
svn0202 <svn0202@users.noreply.github.com>
2026-08-09 20:12:52 +0500

    Declare table header parameter types

--- shared/code/tce_functions_general.php
+++ shared/code/tce_functions_general.php
@@ -718,7 +718,7 @@ function f_html_to_tsv($str): mixed
  * @param $filter (string) additional parameters to pass on URL
  * @return string table header element
  */
-function F_select_table_header_element($order_field, $orderdir, $title, $name, $current_order_field = '', $filter = ''): string
+function F_select_table_header_element(mixed $order_field, mixed $orderdir, mixed $title, mixed $name, mixed $current_order_field = '', mixed $filter = ''): string
 {
     global $l;
     require_once '../config/tce_config.php';
