03c731ed247cdd9229cacf1870fb5852d48f4237
svn0202 <svn0202@users.noreply.github.com>
2026-08-10 03:37:48 +0500

    Test user selection wrapper

--- test/UserSelectFunctionsTest.php
+++ test/UserSelectFunctionsTest.php
@@ -6,6 +6,35 @@ use PHPUnit\Framework\TestCase;
 
 final class UserSelectFunctionsTest extends TestCase
 {
+    public function testSelectUserWrapperForwardsArgumentsAndReturnsTrue(): void
+    {
+        [$status, $output] = \F_tcecode_run_process(
+            [
+                PHP_BINARY,
+                '-r',
+                'namespace Harness; $GLOBALS["arguments"] = []; '
+                    . 'function F_show_select_user(...$arguments) { $GLOBALS["arguments"] = $arguments; } '
+                    . '$source = file_get_contents($argv[1]); '
+                    . 'preg_match("/function [Ff]_select_user\\(/", $source, $match, PREG_OFFSET_CAPTURE); '
+                    . '$start = $match[0][1]; $end = strpos($source, "\\n/**", $start); '
+                    . 'eval("namespace Harness; " . substr($source, $start, $end - $start)); '
+                    . '$result = F_select_user("name", "DESC", "4", "25", 7, "active=1", "Ada"); '
+                    . 'echo json_encode(["result" => $result, "arguments" => $GLOBALS["arguments"]]);',
+                dirname(__DIR__) . '/admin/code/tce_functions_user_select.php',
+            ],
+            dirname(__DIR__) . '/admin/code',
+        );
+
+        self::assertSame(0, $status, $output);
+        self::assertSame(
+            [
+                'result' => true,
+                'arguments' => ['name', 'DESC', '4', '25', 7, 'active=1', 'Ada'],
+            ],
+            json_decode($output, true, 512, JSON_THROW_ON_ERROR),
+        );
+    }
+
     public function testUserGroupSelectRenderingRemainsUnchanged(): void
     {
         [$status, $output] = \F_tcecode_run_process(
