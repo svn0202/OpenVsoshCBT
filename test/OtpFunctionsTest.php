47519de1fb48a481135f8747d81925da4552202a
svn0202 <svn0202@users.noreply.github.com>
2026-08-09 13:52:08 +0500

    Type OTP secret generation

--- test/OtpFunctionsTest.php
+++ test/OtpFunctionsTest.php
@@ -8,6 +8,14 @@ require_once __DIR__ . '/../shared/code/tce_functions_otp.php';
 
 final class OtpFunctionsTest extends TestCase
 {
+    public function testRandomKeyUsesSixteenBase32Characters(): void
+    {
+        $key = \F_getRandomOTPkey();
+
+        self::assertSame(16, strlen($key));
+        self::assertMatchesRegularExpression('/^[A-Z2-7]{16}$/', $key);
+    }
+
     public function testBase32DecoderMatchesKnownValue(): void
     {
         self::assertSame('foo', \F_decodeBase32('MZXW6==='));
