208ed2feb50f347b2c186ba76832f8be194ae2e3
svn0202 <svn0202@users.noreply.github.com>
2026-08-09 20:00:05 +0500

    Declare object replacement return type

--- test/TcecodeFunctionsTest.php
+++ test/TcecodeFunctionsTest.php
@@ -41,6 +41,31 @@ final class TcecodeFunctionsTest extends TestCase
         self::assertSame('"A &amp; B"', $output);
     }
 
+    public function testObjectReplacementReturnsImageMarkup(): void
+    {
+        [$status, $output] = \F_tcecode_run_process(
+            [
+                PHP_BINARY,
+                '-r',
+                'require_once "../config/tce_config.php"; require_once "tce_functions_tcecode.php"; '
+                    . '$name = "tce-object-test-" . hrtime(true); $path = K_PATH_CACHE . $name . ".png"; '
+                    . 'file_put_contents($path, base64_decode('
+                    . '"iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAusB9Y9Z8ZkAAAAASUVORK5CYII=")); '
+                    . '$maxWidth = 0; $maxHeight = 0; try { '
+                    . '$markup = F_objects_replacement($name, "png", 10, 20, "", $maxWidth, $maxHeight); '
+                    . 'echo json_encode(str_replace($name, "{name}", $markup)); } finally { unlink($path); }',
+            ],
+            dirname(__DIR__) . '/shared/code',
+        );
+
+        self::assertSame(0, $status);
+        self::assertSame(
+            '"<img src=\"\\/cache\\/{name}.png\" alt=\"image:{name}.png\" width=\"10\" height=\"20\" '
+                . 'class=\"tcecode\" \\/>"',
+            $output,
+        );
+    }
+
     public function testStringTransformersPreserveTcecodeRendering(): void
     {
         $this->assertSame(
