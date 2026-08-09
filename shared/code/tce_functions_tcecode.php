5eb02a56d209ae74ff83b6fcedff0cb2027cf99e
svn0202 <svn0202@users.noreply.github.com>
2026-08-09 19:33:41 +0500

    Rename TCECode line helper

--- shared/code/tce_functions_tcecode.php
+++ shared/code/tce_functions_tcecode.php
@@ -940,7 +940,7 @@ function f_remove_tcecode($str): mixed
  * @param $str (string) text to process
  * return string
  */
-function F_tcecodeToLine($str): mixed
+function f_tcecode_to_line($str): mixed
 {
     $str = (string) $str;
     if (F_has_html_markup($str)) {
