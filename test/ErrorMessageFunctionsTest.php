973edfe0f1ff37b4c95213473e0cab6c1a844b8d
svn0202 <svn0202@users.noreply.github.com>
2026-08-09 21:14:11 +0500

    Declare PHP error handler types

--- test/ErrorMessageFunctionsTest.php
+++ test/ErrorMessageFunctionsTest.php
@@ -6,6 +6,23 @@ use PHPUnit\Framework\TestCase;
 
 final class ErrorMessageFunctionsTest extends TestCase
 {
+    public function testErrorHandlerReturnsNothingWhenReportingIsDisabled(): void
+    {
+        [$status, $output] = \F_tcecode_run_process(
+            [
+                PHP_BINARY,
+                '-r',
+                'define("K_ERROR_TYPES", E_ALL); require "tce_functions_errmsg.php"; '
+                    . 'error_reporting(0); '
+                    . 'echo json_encode(F_error_handler(E_WARNING, "ignored", __FILE__, __LINE__));',
+            ],
+            dirname(__DIR__) . '/shared/code',
+        );
+
+        self::assertSame(0, $status);
+        self::assertSame('null', $output);
+    }
+
     public function testFileExistsWrapperHandlesLocalAndUnsupportedPaths(): void
     {
         [$status, $output] = \F_tcecode_run_process(
