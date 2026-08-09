11294b5ca3d86befd2753aa0456a35616f94113d
svn0202 <svn0202@users.noreply.github.com>
2026-08-09 19:39:14 +0500

    Declare array statistics return type

--- shared/code/tce_functions_statistics.php
+++ shared/code/tce_functions_statistics.php
@@ -27,7 +27,7 @@
  * @param $data (array) input data as bidimesional array. The first dimension is a set of data, the second contains data.
  * @return array of statistical results. The keys of the input data are peserved.
  */
-function F_getArrayStatistics($data)
+function F_getArrayStatistics($data): mixed
 {
     $stats = [];
     $stats['number'] = []; // number of items
