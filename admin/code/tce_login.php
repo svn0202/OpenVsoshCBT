4e8518353187c3616a4e5b9fcae996e5c0cc9d6a
svn0202 <svn0202@users.noreply.github.com>
2026-08-10 12:12:05 +0500

    Type admin login translations

--- admin/code/tce_login.php
+++ admin/code/tce_login.php
@@ -22,6 +22,7 @@
  */
 
 require_once '../config/tce_config.php';
+/** @var array{a_meta_language: string, a_meta_dir: string, a_meta_charset: string, w_login: string} $l */
 
 $pagelevel = 1;
 require_once '../../shared/code/tce_authorization.php';
