e8fbb4c5967caeb24274fe89e93fdbc66949782e
svn0202 <svn0202@users.noreply.github.com>
2026-08-10 12:32:14 +0500

    Type admin logout redirect

--- admin/code/tce_logout.php
+++ admin/code/tce_logout.php
@@ -22,6 +22,7 @@
 
 require_once '../config/tce_config.php';
 require_once '../../shared/code/tce_functions_session.php';
+/** @var array{a_meta_language: string, a_meta_dir: string, a_meta_charset: string, w_logout: string} $l */
 
 // Destroys all user's session data
 session_unset();
@@ -36,7 +37,7 @@ setcookie('PHPSESSID', '', [
     'samesite' => K_COOKIE_SAMESITE,
 ]);
 
-if (!isset($current_page)) {
+if (!isset($current_page) || !is_string($current_page)) {
     $current_page = '../code/index.php?logout=1';
 } elseif (!str_contains($current_page, '?')) {
     $current_page .= '?logout=1';
