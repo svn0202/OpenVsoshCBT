51b63f9aac4b93c26d6c2f99a3e96098cba7be94
svn0202 <svn0202@users.noreply.github.com>
2026-08-10 05:29:45 +0500

    Test ordered test SSL certificate ID list

--- test/TestReviewTest.php
+++ test/TestReviewTest.php
@@ -240,6 +240,40 @@ final class TestReviewTest extends TestCase
         );
     }
 
+    public function testTestSslCertificatesReturnOrderedCommaSeparatedIds(): void
+    {
+        [$status, $output] = \F_tcecode_run_process(
+            [
+                PHP_BINARY,
+                '-r',
+                'namespace Harness; define("K_TABLE_TEST_SSLCERTS", "test_ssl"); '
+                    . '$GLOBALS["db"] = "db"; $GLOBALS["rows"] = [['
+                    . '"tstssl_ssl_id" => 2], ["tstssl_ssl_id" => 8]]; $GLOBALS["queries"] = []; '
+                    . 'function F_db_query($sql, $db) { $GLOBALS["queries"][] = $sql; return true; } '
+                    . 'function F_db_fetch_assoc($result) { return array_shift($GLOBALS["rows"]); } '
+                    . '$source = file_get_contents($argv[1]); '
+                    . 'preg_match("/function (F_getTestSSLCerts|f_get_test_ssl_certs)\\(/", '
+                    . '$source, $match, PREG_OFFSET_CAPTURE); '
+                    . '$name = $match[1][0]; $start = $match[0][1]; '
+                    . '$end = strpos($source, "\\n/**", $start); '
+                    . '$function = substr($source, $start, $end - $start); '
+                    . '$function = preg_replace("/^\\s*require_once [^;]+;\\n/m", "", $function); '
+                    . 'eval("namespace Harness; " . $function); '
+                    . '$qualified = __NAMESPACE__ . "\\\\" . $name; '
+                    . 'echo json_encode([$qualified("7"), $GLOBALS["queries"]]);',
+                dirname(__DIR__) . '/shared/code/tce_functions_test.php',
+            ],
+            dirname(__DIR__) . '/shared/code',
+        );
+
+        self::assertSame(0, $status, $output);
+        self::assertSame(
+            ['0,2,8', ['SELECT tstssl_ssl_id FROM test_ssl WHERE tstssl_test_id=7 ORDER BY tstssl_ssl_id']],
+            json_decode($output, true, 512, JSON_THROW_ON_ERROR),
+        );
+    }
+
+
 
 
 
