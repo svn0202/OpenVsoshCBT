01c02bfa1034bd55f36367f0a75c295ed8c26555
svn0202 <svn0202@users.noreply.github.com>
2026-08-10 05:56:28 +0500

    Test generated test statistics insert

--- test/TestReviewTest.php
+++ test/TestReviewTest.php
@@ -582,6 +582,47 @@ final class TestReviewTest extends TestCase
         }
     }
 
+    public function testGeneratedTestStatisticsPreserveInsertAndErrorHandling(): void
+    {
+        [$status, $output] = \F_tcecode_run_process(
+            [
+                PHP_BINARY,
+                '-r',
+                'namespace Harness; define("K_TABLE_TESTUSER_STAT", "test_stats"); $GLOBALS["db"] = "db"; '
+                    . '$GLOBALS["results"] = [true, false]; $GLOBALS["queries"] = []; $GLOBALS["errors"] = 0; '
+                    . 'function F_db_query($sql, $db) { $GLOBALS["queries"][] = $sql; '
+                    . 'return array_shift($GLOBALS["results"]); } '
+                    . 'function F_display_db_error() { ++$GLOBALS["errors"]; } '
+                    . '$source = file_get_contents($argv[1]); '
+                    . 'preg_match("/function (F_updateTestuserStat|f_update_testuser_stat)\\(/", '
+                    . '$source, $match, PREG_OFFSET_CAPTURE); '
+                    . '$name = $match[1][0]; $start = $match[0][1]; '
+                    . '$end = strpos($source, "\\n/**", $start); '
+                    . '$function = substr($source, $start, $end - $start); '
+                    . '$function = preg_replace("/^\\s*require_once [^;]+;\\n/m", "", $function); '
+                    . 'eval("namespace Harness; " . $function); '
+                    . '$qualified = __NAMESPACE__ . "\\\\" . $name; '
+                    . 'echo json_encode([[$qualified("2026-01-02 03:04:05"), '
+                    . '$qualified("2026-06-07 08:09:10")], $GLOBALS["errors"], $GLOBALS["queries"]]);',
+                dirname(__DIR__) . '/shared/code/tce_functions_test.php',
+            ],
+            dirname(__DIR__) . '/shared/code',
+        );
+
+        self::assertSame(0, $status, $output);
+        self::assertSame(
+            [
+                [null, null],
+                1,
+                [
+                    "INSERT INTO test_stats (tus_date) VALUES ('2026-01-02 03:04:05')",
+                    "INSERT INTO test_stats (tus_date) VALUES ('2026-06-07 08:09:10')",
+                ],
+            ],
+            json_decode($output, true, 512, JSON_THROW_ON_ERROR),
+        );
+    }
+
 
 
 
