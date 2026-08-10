a1068c9e2ed1d3b9c598b76c1f5667898586d704
svn0202 <svn0202@users.noreply.github.com>
2026-08-10 14:51:35 +0500

    Type password change controller

--- public/code/tce_user_change_password.php
+++ public/code/tce_user_change_password.php
@@ -30,13 +30,33 @@ $newpassword_repeat = isset($_POST['newpassword_repeat']) && is_string($_POST['n
     ? $_POST['newpassword_repeat']
     : '';
 
+/** @var int $pagelevel */
 $pagelevel = K_AUTH_USER_CHANGE_PASSWORD;
+/** @var array{
+ *     t_user_change_password: string,
+ *     w_current_password: string,
+ *     w_new_password: string,
+ *     a_meta_charset: string,
+ *     m_different_passwords: string,
+ *     m_login_wrong: string,
+ *     m_password_updated: string,
+ *     h_password: string,
+ *     d_password_length: string,
+ *     h_password_repeat: string,
+ *     w_repeat: string,
+ *     w_update: string,
+ *     h_update: string,
+ *     hp_user_change_password: string
+ * } $l
+ */
+/** @var mixed $db */
 $thispage_title = $l['t_user_change_password'];
 require_once '../../shared/code/tce_authorization.php';
 require_once '../../shared/config/tce_user_registration.php';
 require_once '../../shared/code/tce_functions_form.php';
 require_once '../code/tce_page_header.php';
 
+/** @mago-expect analysis:possibly-undefined-string-array-index */
 $user_id = (int) $_SESSION['session_user_id'];
 
 // comma separated list of required fields
@@ -48,6 +68,7 @@ $_REQUEST['ff_required_labels'] = htmlspecialchars(
 );
 
 // process submitted data
+/** @var string $menu_mode */
 switch ($menu_mode) {
     case 'update': // Update user
         if ($formstatus = F_check_form_fields()) {
@@ -62,9 +83,11 @@ switch ($menu_mode) {
             }
 
             $sql = 'SELECT user_password FROM ' . K_TABLE_USERS . ' WHERE user_id=' . $user_id;
-            if ($r = F_db_query($sql, $db)) {
+            $r = f_tmf_change_password_query_result(F_db_query($sql, $db));
+            if ($r) {
+                $m = f_tmf_change_password_row(F_db_fetch_array($r));
                 if (
-                    !($m = F_db_fetch_array($r))
+                    $m === null
                     || !check_password($currentpassword, (string) ($m['user_password'] ?? ''))
                 ) {
                     F_print_error('WARNING', $l['m_login_wrong']);
@@ -86,7 +109,8 @@ switch ($menu_mode) {
                 . '\'
 				WHERE user_id='
                 . $user_id;
-            if (!($r = F_db_query($sql, $db))) {
+            $r = f_tmf_change_password_query_result(F_db_query($sql, $db));
+            if (!$r) {
                 F_display_db_error(false);
             } else {
                 F_print_error('MESSAGE', $l['m_password_updated']);
@@ -174,3 +198,23 @@ echo '<div class="pagehelp">' . $l['hp_user_change_password'] . '</div>' . K_NEW
 echo '</div>' . K_NEWLINE;
 
 require_once __DIR__ . '/tce_page_footer.php';
+
+/** @return array<array-key, mixed>|null */
+function f_tmf_change_password_row(mixed $row): ?array
+{
+    return is_array($row) ? $row : null;
+}
+
+/** @return \mysqli_result|\PgSql\Result|resource|bool */
+function f_tmf_change_password_query_result(mixed $result): mixed
+{
+    if (
+        is_bool($result)
+        || is_resource($result)
+        || $result instanceof \mysqli_result
+        || $result instanceof \PgSql\Result
+    ) {
+        return $result;
+    }
+    return false;
+}
