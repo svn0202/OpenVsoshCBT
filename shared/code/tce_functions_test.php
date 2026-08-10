8869bcac65ed58f93146561cab2a84661cbb362d
svn0202 <svn0202@users.noreply.github.com>
2026-08-10 05:22:00 +0500

    Rename omitted question count helper

--- shared/code/tce_functions_test.php
+++ shared/code/tce_functions_test.php
@@ -3042,7 +3042,7 @@ function f_tmf_live_score(int $test_id, int $testuser_id): ?float
  * @param $test_id (int) test ID
  * @return integer number
  */
-function F_getNumOmittedQuestions($test_id)
+function f_get_num_omitted_questions($test_id)
 {
     require_once '../config/tce_config.php';
     global $db, $l;
