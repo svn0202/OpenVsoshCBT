609e7be26db7ccd302bd14f3c5f9a0bd021a767d
svn0202 <svn0202@users.noreply.github.com>
2026-08-10 05:41:33 +0500

    Test user statistics order filtering

--- test/StatisticsTest.php
+++ test/StatisticsTest.php
@@ -8,6 +8,41 @@ require_once __DIR__ . '/../shared/code/tce_functions_statistics.php';
 
 final class StatisticsTest extends TestCase
 {
+    public function testUserTestStatisticsOrderByFiltersAndFormatsInput(): void
+    {
+        [$status, $output] = \F_tcecode_run_process(
+            [
+                PHP_BINARY,
+                '-r',
+                'namespace Harness; $source = file_get_contents($argv[1]); '
+                    . 'preg_match("/function (F_getSafeUsersTestStatOrderBy|f_get_safe_users_test_stat_order_by)\\(/", '
+                    . '$source, $match, PREG_OFFSET_CAPTURE); '
+                    . '$name = $match[1][0]; $start = $match[0][1]; '
+                    . '$end = strpos($source, "\\n/**", $start); '
+                    . 'eval("namespace Harness; " . substr($source, $start, $end - $start)); '
+                    . '$qualified = __NAMESPACE__ . "\\\\" . $name; '
+                    . 'echo json_encode(['
+                    . '$qualified(" user_lastname desc , TOTAL_SCORE, malicious; DROP, user_name ASC, '
+                    . 'testuser_end_time DeSc "), '
+                    . '$qualified("unknown ASC, total_score DESC"), '
+                    . '$qualified(""), $qualified(null)]);',
+                dirname(__DIR__) . '/shared/code/tce_functions_test_stats.php',
+            ],
+            dirname(__DIR__) . '/shared/code',
+        );
+
+        self::assertSame(0, $status, $output);
+        self::assertSame(
+            [
+                'user_lastname DESC, total_score, testuser_end_time DESC',
+                'total_score DESC',
+                'total_score, user_lastname, user_firstname',
+                'total_score, user_lastname, user_firstname',
+            ],
+            json_decode($output, true, 512, JSON_THROW_ON_ERROR),
+        );
+    }
+
     public function testEvenMedianAndStandardDeviationBranches(): void
     {
         /**
