638a54453da4beb2877fa2d33d3dbe220247519e
svn0202 <svn0202@users.noreply.github.com>
2026-08-10 13:11:28 +0500

    Test extensionless upload names

--- test/UploadFunctionsTest.php
+++ test/UploadFunctionsTest.php
@@ -49,14 +49,15 @@ final class UploadFunctionsTest extends TestCase
             [
                 PHP_BINARY,
                 '-r',
-                'define("K_ALLOWED_UPLOAD_EXTENSIONS", serialize(["jpg", "png"])); '
+                'error_reporting(0); define("K_ALLOWED_UPLOAD_EXTENSIONS", serialize(["jpg", "png"])); '
                     . 'require "tce_functions_upload.php"; '
-                    . 'echo json_encode([f_is_allowed_upload("photo.JPG"), f_is_allowed_upload("shell.php")]);',
+                    . 'echo json_encode([f_is_allowed_upload("photo.JPG"), '
+                    . 'f_is_allowed_upload("shell.php"), f_is_allowed_upload("README")]);',
             ],
             __DIR__ . '/../admin/code',
         );
 
         self::assertSame(0, $status);
-        self::assertSame('[true,false]', $output);
+        self::assertSame('[true,false,false]', $output);
     }
 }
