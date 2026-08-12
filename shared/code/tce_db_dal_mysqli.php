427021a5d255e02b9b8a0106c10414955a1da13a
svn0202 <svn0202@users.noreply.github.com>
2026-08-12 11:13:42 +0500

    Type unused MySQLi insert table

--- shared/code/tce_db_dal_mysqli.php
+++ shared/code/tce_db_dal_mysqli.php
@@ -175,9 +175,10 @@ function f_db_num_rows(mixed $result): mixed
  * @return int ID generated from the last INSERT operation.
  */
 // @mago-expect analysis:duplicate-definition -- only one configured DAL implementation is loaded at runtime
-function f_db_insert_id(mixed $link_identifier, $tablename = '', $fieldname = '')
+function f_db_insert_id(mixed $link_identifier, mixed $tablename = '', $fieldname = '')
 {
     /** @var mysqli $link_identifier */
+    unset($tablename);
     return mysqli_insert_id($link_identifier);
 }
 
