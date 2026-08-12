b51444a1d67e550370a44a705bf2c14c8be876ee
svn0202 <svn0202@users.noreply.github.com>
2026-08-12 10:02:19 +0500

    Type legacy MySQL insert table

--- shared/code/tce_db_dal_mysql.php
+++ shared/code/tce_db_dal_mysql.php
@@ -175,6 +175,7 @@ function f_db_num_rows(mixed $result): mixed
 // @mago-expect analysis:duplicate-definition -- only one configured DAL implementation is loaded at runtime
 function f_db_insert_id(mixed $link_identifier, mixed $tablename = '', mixed $fieldname = ''): mixed
 {
+    /** @var string $tablename */
     unset($fieldname);
     /*
      * NOTE : mysql_insert_id() converts the return type of the
