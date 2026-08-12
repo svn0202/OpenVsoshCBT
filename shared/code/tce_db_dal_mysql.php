c6bf1834ffedb5cc62facb5ffb0ca7f4f15ba4b3
svn0202 <svn0202@users.noreply.github.com>
2026-08-12 09:35:43 +0500

    Type legacy MySQL escaped string

--- shared/code/tce_db_dal_mysql.php
+++ shared/code/tce_db_dal_mysql.php
@@ -209,6 +209,7 @@ function f_db_datetime_diff_seconds(mixed $start_date_field, mixed $end_date_fie
 // @mago-expect analysis:duplicate-definition -- only one configured DAL implementation is loaded at runtime
 function f_escape_sql(mixed $link_identifier, mixed $str, mixed $stripslashes = true): mixed
 {
+    /** @var string $str */
     // Reverse magic_quotes_gpc/magic_quotes_sybase effects if ON.
     if ($stripslashes) {
         $str = stripslashes($str);
